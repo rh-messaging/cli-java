1c1bd2d0900009ee58a0e4d02e8d182fee168a60
Jiri Danek <jdanek@redhat.com>
2017-10-25 13:37:20 +0200

    Unify lib/ClientOptionManager and jms/ClientOptionManager

--- cli-qpid-jms/src/main/java/com/redhat/mqe/jms/AacConnectorOptions.java
+++ cli-qpid-jms/src/main/java/com/redhat/mqe/jms/AacConnectorOptions.java
@@ -19,6 +19,7 @@
 
 package com.redhat.mqe.jms;
 
+import com.redhat.mqe.lib.ClientOptionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,9 +30,9 @@ import java.util.List;
 /**
  * Default options for ConnectorClient.
  */
-public class ConnectorOptions extends ClientOptions {
+public class AacConnectorOptions extends AacClientOptions {
     private List<com.redhat.mqe.lib.Option> options = new LinkedList<com.redhat.mqe.lib.Option>();
-    private Logger LOG = LoggerFactory.getLogger(ReceiverOptions.class);
+    private Logger LOG = LoggerFactory.getLogger(AacReceiverOptions.class);
     private final List<com.redhat.mqe.lib.Option> connectorDefaultOptions = new LinkedList<com.redhat.mqe.lib.Option>();
 
     {
@@ -45,7 +46,7 @@ public class ConnectorOptions extends ClientOptions {
         ));
     }
 
-    ConnectorOptions() {
+    AacConnectorOptions() {
         this.options = ClientOptionManager.mergeOptionLists(super.getDefaultOptions(), connectorDefaultOptions);
     }
 
