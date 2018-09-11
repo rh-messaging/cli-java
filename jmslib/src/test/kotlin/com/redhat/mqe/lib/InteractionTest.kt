f5735ff5f0b498efc9e49458bffe9d57f88d7004
Jiri Danek <jdanek@redhat.com>
2018-09-11 19:45:58 +0200

    Add a Mockito based test
    Mockito is a mock/spy generation framework. It makes it easier to
    replace some objects with test doubles (mocks, spies), then run a program
    in a test, and assert about behavior of the code (what methods of the
    mocks/spies were called, with what arguments, ...)
    
    This is similar to Byteman, except that the code being tested needs
    to be written in a way compatible with mocking. (It must be possible to
    pass the mocked objects as constructor/method parameters. Instance
    created using `new` cannot be mocked by Mockito.)
    
    Thinking of qe clis as a program that transforms a set of cli options
    into JMS API calls, we can use this to check that a given option line
    results in the correct api calls. These tests should be faster to run
    than end-to-end send/receive using real broker. Embedded or otherwise.

---
+++ jmslib/src/test/kotlin/com/redhat/mqe/lib/InteractionTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.redhat.mqe.lib
+
+import com.redhat.mqe.lib.Main.main
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.*
+import javax.jms.*
+
+class InteractionTest {
+    @Test
+    fun `test run sender no params`() {
+        val producer = mock(MessageProducer::class.java)
+
+        val message = mock(Message::class.java)
+
+        val session = mock(Session::class.java)
+        `when`(session.createProducer(any(Destination::class.java)))
+            .thenReturn(producer)
+        `when`(session.createProducer(null))
+            .thenReturn(producer)
+        given(session.createMessage()).willReturn(message)
+
+        val connection = mock(Connection::class.java)
+        `when`(connection.createSession(anyBoolean(), anyInt()))
+            .thenReturn(session)
+
+        val connectionManager = mock(ConnectionManager::class.java)
+        given(connectionManager.getConnection()).willReturn(connection)
+
+        val connectionManagerFactory = mock(ConnectionManagerFactory::class.java)
+        `when`(connectionManagerFactory.make(any(ClientOptions::class.java), anyString()))
+            .thenReturn(connectionManager)
+
+        val args = arrayOf("sender")
+        val client = DaggerFakeClient.builder()
+            .connectionManagerFactory(connectionManagerFactory)
+            .messageFormatter(mock(JmsMessageFormatter::class.java))
+            .clientOptionManager(mock(ClientOptionManager::class.java))
+            .args(args)
+            .build()
+
+        // may need changing, put in mock ClientOptions instead?
+        main(args, client)  // or client.makeSenderClient().startClient()?
+
+        verify(producer).send(message)
+    }
+}
