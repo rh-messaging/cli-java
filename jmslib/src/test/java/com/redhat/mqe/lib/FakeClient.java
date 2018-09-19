dfdb746722509a4e650533234e44911e92d70a27
Jiří Daněk <jdanek@redhat.com>
2018-09-19 10:52:00 +0200

    Add a Mockito based test (#87)
    * Add a Mockito based test
    
    Mockito is a mock/spy generation framework. It makes it easier to
    replace some objects with test doubles (mocks, spies), then run a program
    in a test, and assert about behavior of the code (what methods of the
    mocks/spies were called, with what arguments, ...)
    
    This is similar to Byteman, except that the code being tested needs
    to be written in a way compatible with mocking. (It must be possible to
    pass the mocked objects as constructor/method parameters. Instance
    created using `new` cannot be mocked by Mockito.)
    
    Thinking of qe clis as a program that transforms a set of cli options
    into JMS API calls, we can use this to check that a given option line
    results in the correct api calls. These tests should be faster to run
    than end-to-end send/receive using real broker. Embedded or otherwise.
    
    * fixup Add forgotten mockito config
    
    * fixup `when`
    
    * fixup remove todo comment

---
+++ jmslib/src/test/java/com/redhat/mqe/lib/FakeClient.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.redhat.mqe.lib;
+
+import com.redhat.mqe.ClientListener;
+import dagger.Binds;
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.Module;
+
+import javax.annotation.Nullable;
+import javax.inject.Named;
+
+
+@Component(modules = {FakeClientModule.class})
+interface FakeClient extends Client {
+    ConnectionManagerFactory fakeConnectionManagerFactory();
+
+    @Component.Builder
+    abstract class Builder {
+        @BindsInstance
+        abstract Builder connectionManagerFactory(ConnectionManagerFactory f);
+
+        @BindsInstance
+        abstract Builder messageFormatter(JmsMessageFormatter f);
+
+        @BindsInstance
+        abstract Builder clientOptionManager(ClientOptionManager m);
+
+        @BindsInstance
+        abstract Builder args(@Args String[] args);
+
+        @BindsInstance
+        abstract Builder listener(@Nullable ClientListener listener);
+
+        abstract FakeClient build();
+    }
+}
+
+@Module(includes = FakeClientModule.Declarations.class)
+class FakeClientModule {
+    @Module
+    interface Declarations {
+        @Binds
+        @Named("Sender")
+        ClientOptions bindClientOptions(SenderOptions o);
+
+        @Binds
+        @Named("Receiver")
+        ClientOptions bindReceiverOptions(ReceiverOptions o);
+
+        @Binds
+        @Named("Connector")
+        ClientOptions bindConnectorOptions(ConnectorOptions o);
+    }
+}
